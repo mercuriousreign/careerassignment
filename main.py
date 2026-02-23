5f70466ca9e2f34f8fd1de024a03264f68a0234c
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2026-02-23 16:24:02 -0500

    changes to main

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ origins = [
     "http://127.0.0.1:5173",
 ]
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
